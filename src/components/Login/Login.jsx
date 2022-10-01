0b6d6bb43f9ce18e336e81efe404dec94a949f6b
raquelfernandezbermudez <33099186+raquelfernandezbermudez@users.noreply.github.com>
2022-10-02 00:26:20 +0200

    Componente login, estados, api y formulario

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,27 +1,66 @@
-
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import { Axios } from 'axios';
-import React, { Component } from 'react';
-import './Login.css';
+import React, { Component } from 'react'
+import axios from 'axios';
 
 export class Login extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: 'Escribe tu email',
+      password: 'Escribe tu contraseña'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }//Constructor
+
+  // Gestionar los cambios en cualquier elemento del form
+  handleChange(event) {
+    if(event.target.name === 'email') {
+      this.setState({
+        email: event.target.value,
+        password: this.state.password
+      })    
+    } else if(event.target.name === 'password') {
+      this.setState({
+        email: this.state.email,
+        password: event.target.value
+      })    
+    }
+  }
+ 
+  handleSubmit(event) {
+    axios.post('http://51.38.51.187:5050/api/v1/auth/log-in', {
+        email: this.state.email,
+        password: this.state.password,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+      event.preventDefault();
+    }
+
   render() {
-
-  //   const prueba = async () => {
-  //     console.log('hola')
-  // }
-
-
-  async function logInUser() {
-  
-}
-
     return (
       <div>
-    
-        Login
-        {/* <button onClick={signUpUser}>Nuevo usuario</button> &nbsp;
-        <button onClick={logInUser}>Ingreso</button> &nbsp; */}
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <p>Email</p>
+            <input type="text" name='email' onChange={this.handleChange}/>
+          </label>
+          <label>
+            <p>Contraseña</p>
+            <input type="password"  name='password' onChange={this.handleChange}/>
+          </label>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+
       </div>
     )
   }
